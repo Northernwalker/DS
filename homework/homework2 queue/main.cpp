59defc780304bf93399b3697cea29caa2dcb0824
Northernwalker <127959637+Northernwalker@users.noreply.github.com>
2023-04-17 19:15:25 +0800

    Update main.cpp

--- homework/homework2 queue/main.cpp
+++ homework/homework2 queue/main.cpp
@@ -26,17 +26,26 @@ int largestRectangleArea(vector<int>& heights) {
 
 int main() {
     srand(time(0));
-    int s[10];//创建数组储存每个柱型的高度，此处定为10个柱型
+    int s1[10];//创建数组储存每个柱型的高度，此处定为10个柱型
     for (int i = 0; i < 10; i++) {
         cout << "每个柱型高度：";
         for (int j = 0; j < 10; j++) {
-            s[j] = rand() % 100;//为每个柱型赋随机值
-            cout << s[j] << " ";
+            s1[j] = rand() % 100;//为每个柱型赋随机值
+            cout << s1[j] << " ";
         }
         cout << endl;
-        vector<int> heights = { s[0],s[1],s[2],s[3],s[4],s[5],s[6],s[7],s[8],s[9] };
+        vector<int> heights = { s1[0],s1[1],s1[2],s1[3],s1[4],s1[5],s1[6],s1[7],s1[8],s1[9] };
         int res = largestRectangleArea(heights);//计算最小面积
         cout << res << endl;//输出最小面积
     }
+    //也可手动输入高度（具体个数可在代码中定义，默认为10个）
+    int s2[10] = { 0,0,0,0,0,0,0,0,0,0 };//初始化
+    cout << "请输入高度(10个）：" << endl;
+    for (int k = 0; k < 10; k++) {
+        cin >> s2[k];
+    }
+    vector<int> heights = { s2[0],s2[1],s2[2],s2[3],s2[4],s2[5],s2[6],s2[7],s2[8],s2[9] };
+    int res = largestRectangleArea(heights);//计算最小面积
+    cout << res << endl;//输出最小面积
     return 0;
 }
